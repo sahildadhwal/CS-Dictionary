d41a429764a5b6241245c9d189e6ec261f36fff7
Andrew Jia <andrew.jia.2013@gmail.com>
2022-11-07 22:52:47 -0800

    Update testeslint.js

--- source/testeslint.js
+++ source/testeslint.js
@@ -1,4 +1,4 @@
-//some badly formatted code
+/* //some badly formatted code
 
     // Function to compute the product of p1 and p2
 function myFunction(p1, p2) {
@@ -6,4 +6,4 @@ function myFunction(p1, p2) {
      return product;
   }
   console.log(myFunction(2, 2)) ;
-  let hiIamError;
+  let hiIamError; */
