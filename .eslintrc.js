f5bef0dc8e41f110db90596c774125a47ee6a426
Amaar <avallian@ucsd.edu>
2022-11-13 17:20:17 -0800

    updated styling of eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
-    "jest/globals": true
+    'jest/globals': true,
   },
   extends: 'airbnb-base',
   overrides: [
@@ -14,5 +14,5 @@ module.exports = {
   },
   rules: {
   },
-  "plugins": ["jest"],
+  plugins: ['jest'],
 };
