44069b1fd36fc651cd195c947107f7bbb5797424
Chieh0501 <chung@ucsd.edu>
2022-11-19 15:21:22 -0800

    CHanged redirection.js function names to camel case and modified quotes

--- .history/source/src/common-scripts/redirection_20221119152035.js
+++ .history/source/src/common-scripts/redirection_20221119152035.js
@@ -1,15 +1,15 @@
-function jumpCreateTermHtml() {
+function jump_CreateTermHtml() {
   location.href='create-term.html';
 }
-function jumpHomeHtml() {
+function jump_HomeHtml() {
   location.href='home.html';
 }
-function jumpPostHtml() {
+function jump_PostHtml() {
   location.href='post.html';
 }
-function jumpSettingsHtml() {
+function jump_SettingsHtml() {
   location.href='settings.html';
 }
-function jumpSearchHtml() {
+function jump_SearchHtml() {
   location.href='search.html';
 }
