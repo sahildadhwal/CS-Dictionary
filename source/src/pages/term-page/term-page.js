569a29c769fbd9c5254198394548ab87316e694d
unknown <lnguyen@ucsd.edu>
2022-11-19 16:25:09 -0800

    Get term data and populate in term page

--- source/src/pages/term-page/term-page.js
+++ source/src/pages/term-page/term-page.js
@@ -1,1 +1,30 @@
-//TODO
+import * as backendFunction from '/src/backend/dict.js'
+window.addEventListener('DOMContentLoaded', init);
+
+let termID = localStorage.getItem('get_term_id');
+let termData = backendFunction.selectTerm(termID);
+let termTitle = document.getElementById('term-title')
+let termDescription = document.getElementById('term-description')
+let tinyMCEContent = document.getElementById('tinyMCE-content')
+let termTags = document.querySelector('.term-tags')
+let publishedDate = document.querySelector('.published-date')
+
+function init() {
+    populateTermData();
+}
+
+function populateTermData() {
+    termTitle.innerHTML = termData['term_name']
+    termDescription.innerHTML = termData['short_description']
+    tinyMCEContent.innerHTML = termData['term_data']
+    
+    let tagList = termData['tags']
+    for(let i = 0; i < tagList.length; i++) {
+        let tagListItem = document.createElement('li');
+        let tagButton = document.createElement('button');
+        tagButton.innerHTML = tagList[i];
+        tagListItem.appendChild(tagButton);
+        termTags.appendChild(tagListItem);
+    }
+    publishedDate.textContent = `Published: ${new Date(termData['created_time']).toLocaleString("en-US")}`;
+}
