1a759af2eb8dfda017c8fa387429e6a21116b045
Amaar <avallian@ucsd.edu>
2022-11-21 19:47:05 -0800

    Update redirection.js

--- source/src/common-scripts/redirection.js
+++ source/src/common-scripts/redirection.js
@@ -1,22 +1,18 @@
-function jumpCreateTermHtml() {
-  location.href='create-term.html';
+function jump_CreateTermHtml() {
+    location.href="create-term.html"
 }
-function jumpHomeHtml() {
-  location.href='home.html';
+function jump_HomeHtml() {
+    location.href="home.html"
 }
-function jumpPostHtml() {
-  location.href='post.html';
+function jump_PostHtml() {
+    location.href="post.html"
 }
-function jumpSettingsHtml() {
-  location.href='settings.html';
+function jump_SettingsHtml() {
+    location.href="settings.html"
 }
-function jumpSearchHtml() {
-  location.href='search.html';
-}
-
 function jump_SearchHtml() {
     location.href="search.html"
-} 
+}
 
 // This is just a temporary function to test the term page
 function jump_TermPageHtml() {
