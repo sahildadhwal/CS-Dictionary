a0638631903a56d6e69a3da0f860975ce91b78ed
sahildadhwal <97374232+sahildadhwal@users.noreply.github.com>
2022-11-22 16:24:03 -0800

    fixing lint errors for search.js

--- source/src/pages/search/search.js
+++ source/src/pages/search/search.js
@@ -3,15 +3,15 @@ window.addEventListener('DOMContentLoaded', init);
 let search_results = JSON.parse(localStorage.getItem('search_results'));
 
 function init() {
-  //Temporarily populating page with recent terms to test how it would visually look
-  //TODO: Replace getDataOfRecents() with whatever search function Backend comes up with
+  // Temporarily populating page with recent terms to test how it would visually look
+  // TODO: Replace getDataOfRecents() with whatever search function Backend comes up with
   // addTermsToDocument(backend_function.getDataOfRecents());
   addTermsToDocument(search_results);
 }
 
 function addTermsToDocument(terms) {
   let recently_added_el = document.querySelector('div.card-results');
-  terms.forEach(term => {
+  terms.forEach((term) => {
     let term_card = document.createElement('term-card');
     term_card.data = term;
     recently_added_el.appendChild(term_card);
