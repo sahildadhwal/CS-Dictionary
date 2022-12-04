ec0bb7058d1274ec0956a0298482adf5cd0d6aa9
Andrew Jia <andrew.jia.2013@gmail.com>
2022-12-03 16:37:43 -0800

    Update e2e.test.js

--- source/test/e2e.test.js
+++ source/test/e2e.test.js
@@ -23,11 +23,6 @@ describe('Basic user flow for Website', () => {
   it('Add a new term', async () => {
     await page.goto('https://cs-dictionary.netlify.app/create-term.html');
 
-    /*
-    const termName = await page.$('#term_name');
-    const termTags = await page.$('#tags');
-    const termShortDescription = await page.$('#short_description');
-    */
     await page.type('#term_name', 'Apple');
     await page.type('#tags', 'fruit, crisp');
     await page.type('#short_description', 'fruit, crisp');
@@ -36,11 +31,32 @@ describe('Basic user flow for Website', () => {
     await page.waitForNavigation();
 
     expect(page.url()).toBe('https://cs-dictionary.netlify.app/home.html');
-    const recentlyAdded = await page.$$('.recently-added-elements>*');
+    const recentlyAdded = await page.$$('.recently-added-elements>*');     
     expect(recentlyAdded.length).toBe(1);
   });
 
-  it('Make a test for the checkpoint video', async () => {
-    expect(2+2).toBe(4); 
+  it('Check term is displayed in recently added', async () => {
+    await page.goto('https://cs-dictionary.netlify.app/');
+
+    const termCards = await page.$$('.recently-added-elements>term-card');
+    expect(termCards.length).toBe(1);                            //only one card in recents
+
+    const termNameEl = await page.evaluateHandle(`document.querySelector("body > main > div.recently-added > div > term-card").shadowRoot.querySelector("#term-name")`);
+    let termName = await page.evaluate(el => el.textContent, termNameEl)
+    expect(termName).toBe('Apple');
+
+    const descriptionEl = await page.evaluateHandle(`document.querySelector("body > main > div.recently-added > div > term-card").shadowRoot.querySelector("#description")`);
+    let description = await page.evaluate(el => el.textContent, descriptionEl)
+    expect(description).toBe('\n        fruit, crisp\n      ');  //newline, 8 spaces, newline, 6 spaces... why
+  });
+
+  it('Check popular tags are working', async () => {
+    const termCards = await page.$$('term-card');
+    console.log(termCards);
+    expect(termCards.length).toBe(11);                           //1 in recents, 2 tags * 5 each, total 11
+    const termNames = await page.evaluateHandle(`document.querySelectorAll("term-card").shadowRoot.querySelector("#term-name")`);
+    let termName0 = await page.evaluate(el => el.textContent, termNames[0]);
+    let termName1 = await page.evaluate(el => el.textContent, termNames[1]);
+    console.log(`${termName0}, ${termName1}`);
   });
 });
