17a58c3358ae68f92644f73e2bf3218fa8ecdc15
Yunxiao Xu <yux029@ucsd.edu>
2022-12-05 12:26:20 -0800

    Fix tinymce eslint

--- source/src/components/tinyMCE/tiny-mce.js
+++ source/src/components/tinyMCE/tiny-mce.js
@@ -19,7 +19,6 @@ export function initNonEditableTinyMCE(selector, data) {
   tinymce.init({
     selector,
     plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
-    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
     tinycomments_mode: 'embedded',
     tinycomments_author: 'Author name',
     mergetags_list: [
